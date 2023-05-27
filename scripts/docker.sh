31fc3d032575f6caa0f029de52a97171dea61719
heart <7362469@qq.com>
2023-05-27 20:50:37 +0800

    chore: add dockerfile sh

---
+++ scripts/docker.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+
+
+docker rm -f fe-playground
+
+docker rmi -f fe-playground:1.0.0
+
+docker build -t fe-playground:1.0.0 .
+
+docker run -p 4001:80 --name fe-playground -d fe-playground:1.0.0
