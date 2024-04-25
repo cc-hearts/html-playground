e9d9147cd7dde6a2f22c75951b69640fa2b02fb6
carl-chen <ccheartszzz@gmail.com>
2024-04-25 20:47:10 +0800

    refactor: optimize layout

--- app.vue
+++ app.vue
@@ -4,19 +4,62 @@
   </NuxtLayout>
 </template>
 
-<style>
+<style lang="scss">
 * {
   margin: 0;
   padding: 0;
 }
 
-:root {
-  --header-height: 48px;
-}
 
-body,html,
-#__nuxt{
+body,
+html,
+#__nuxt {
   width: 100%;
   height: 100%;
+}
+
+body {
+  background: var(--global-bg-color);
+  color: var(--text-color);
+}
+
+
+iframe {
+  border: none;
+}
+
+.splitpanes__splitter {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 4px;
+
+  border-top: 1px solid var(--divider-color);
+  border-bottom: 1px solid var(--divider-color);
+
+
+  &::after,
+  &::before {
+    content: '';
+    width: 32px;
+    height: 1px;
+    background: var(--split-bg-color);
+  }
+}
+
+.splitpanes--vertical {
+  &>.splitpanes__splitter {
+    flex-direction: row;
+    padding: 0 4px;
+    border-left: 1px solid var(--divider-color);
+    border-right: 1px solid var(--divider-color);
+
+    &::after,
+    &::before {
+      width: 1px;
+      height: 32px;
+    }
+  }
 }
 </style>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
+  css: ['assets/scss/theme.scss'],
   modules: ['@unocss/nuxt'],
 })
